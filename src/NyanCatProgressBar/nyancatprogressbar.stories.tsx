d9e2d4af9c8f599af16be1578bfdc9006172eeac
Sergei Zaitsev <zaitsev.sergey.93@gmail.com>
2024-01-02 13:55:55 +0300

    fat: test with ladle

---
+++ src/NyanCatProgressBar/nyancatprogressbar.stories.tsx
@@ -0,0 +1,7 @@
+import { Story } from "@ladle/react";
+import React from "react";
+import NyanCatProgressBar from "./index";
+
+export default { title: "ProgressBar" };
+
+export const ProgressBar: Story = () => <NyanCatProgressBar />;
